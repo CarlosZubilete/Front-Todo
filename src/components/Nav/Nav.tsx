cb9ff3d19fa6292ea61fdbc09a84f3e92a8e6dc0
CarlosZubilete <carloszubilete@gmail.com>
2025-11-28 23:38:05 -0300

    feat(Nav): enhance navigation with responsive toggle and link updates - Added user-select: none to navigation links for better UX. - Implemented responsive navbar toggle functionality using useRef. - Updated navigation links to use React Router's Link component for client-side routing. - Added close button for the responsive navbar. - Included menu button for toggling the navbar visibility.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,8 +1,13 @@
+import { useRef } from "react";
 import "./Nav.css";
+// import * as data from "./links.json";
+import { FaBars, FaMoon, FaSun, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { links } from "./links.json";
-import { FaMoon, FaSun } from "react-icons/fa";
+// const linksString = JSON.stringify(data);
+// const links = JSON.parse(linksString).links;
 
-type Link = {
+type LinkProps = {
   id: number;
   label: string;
   href: string;
@@ -14,6 +19,14 @@ interface DarkModeProps {
 }
 
 const Nav = ({ toggleTheme, isDarkMode }: DarkModeProps) => {
+  // reference to the navbar container
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  const toggleNavbar = () => {
+    if (!navRef.current) return;
+    navRef.current.classList.toggle("navbar__nav--responsive");
+  };
+
   return (
     <nav className="navbar">
       {/* LOGO */}
@@ -27,17 +40,61 @@ const Nav = ({ toggleTheme, isDarkMode }: DarkModeProps) => {
         {isDarkMode ? <FaSun /> : <FaMoon />}
       </button>
       {/* NAVBAR */}
-      <div className="navbar__nav">
-        {links.map((link: Link) => {
+      <div className="navbar__nav" ref={navRef}>
+        {/* NAVBAR BUTTON - RESPONSIVE */}
+        {links.map((link: LinkProps) => {
           return (
-            <div key={link.id} className="navbar__link">
-              <a href={link.href}> {link.label} </a>
-            </div>
+            <Link
+              key={link.id}
+              to={link.href}
+              className="navbar__link"
+              onClick={toggleNavbar}
+            >
+              {link.label}
+            </Link>
           );
         })}
+        <button
+          className="navbar__btn navbar__btn--close"
+          onClick={toggleNavbar}
+        >
+          <FaTimes />
+        </button>
       </div>
+      {/* NAVBAR BUTTON - RESPONSIVE */}
+      <button className="navbar__btn navbar__btn--menu" onClick={toggleNavbar}>
+        <FaBars />
+      </button>
     </nav>
   );
 };
 
 export default Nav;
+
+/*  
+*THIS CODE SHOWS ME A WARMING...
+{links.map((link: LinkProps) => {
+          return (
+            <Link
+              key={link.id}
+              to={link.href}
+              className="navbar__link"
+              onClick={toggleNavbar}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+*/
+
+/* 
+      <Link to="/home" className="navbar__link">
+          Home
+        </Link>
+        <Link to="/home" className="navbar__link">
+          Contact
+        </Link>
+        <Link to="/home" className="navbar__link">
+          Context
+        </Link>
+*/
